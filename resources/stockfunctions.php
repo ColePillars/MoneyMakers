e85ea33545229a453f9919ee9f944f2c79080cea
cWelland96 <cwelland@oakland.edu>
2018-02-05 20:33:25 +0000

    Developed functions to call stock API and store in MYSQL database forv various time series VIA Json

--- resources/stockfunctions.php
+++ resources/stockfunctions.php
@@ -1,7 +1,8 @@
 <?php 
 // this function will fetch and store intradaily values given the parameters
 //RETURNS 100 DATA POINTS, SO IF HOURLY, LAST 100 HOURS!!
-function FetchIntraDaily($StockSymbol, $Interval){
+//CSV is depreciated, use the JSON functions
+function FetchIntraDailyCSV($StockSymbol, $Interval){
     //including db connectoin
     include('connection.php');
   
@@ -37,4 +38,180 @@ function FetchIntraDaily($StockSymbol, $Interval){
         HeaderSkip:
     }
 }
+
+
+// this function will fetch and store intradaily values given the parameters
+//RETURNS 100 DATA POINTS, SO IF HOURLY, LAST 100 HOURS!!
+Function FetchIntraDailyJSON($StockSymbol, $Interval){
+    
+    // include connections page
+    include('connection.php');
+    
+    //Getting the json request into a PHP object
+    $Data = file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" . $StockSymbol . "&interval=". $Interval . "&apikey=S7R0WLCOH163H1DE&datatype=json");
+    
+    //Decoding the json into a php array.
+    $JSONData = json_decode($Data);
+    
+    //Defining base query, will append each row to the query statement
+    $StartSQL = "INSERT INTO StockInfo.Time_Series_Intradaily(atr_stock_ID,Timestamp,Open,High,Low,Close,Volume)
+            VALUES('" . $StockSymbol . "',";
+    
+    //Counter to skip metadata
+    $count = 0;
+    
+    //Looping through each json object
+    foreach ($JSONData  as $JSONObject){
+        //Loop through each day
+        foreach ($JSONObject as $Timestamp => $TimeSeries){
+            //if not meta data, append the time stamp to insert
+            if($count > 5){
+                $MidQuery = $StartSQL;
+                $MidQuery = $MidQuery . "'" . $Timestamp . "',";
+            }
+            //loop through each value, open, high, low, close, volume, append to insert query
+            foreach ($TimeSeries as $ObjectHeader => $ObjectValue){
+                $MidQuery = $MidQuery . "'" . $ObjectValue . "',";
+            }
+            // If not meta data, append end of query statement
+            if($count > 5){
+                $MidQuery =  substr($MidQuery, 0, -2) . "');";
+                if ($conn->query($MidQuery) == TRUE){}
+            }
+            $count = $count + 1;
+        }
+    }   
+}
+
+
+// this function will fetch and store intradaily values given the parameters
+//RETURNS 100 DATA POINTS, SO IF HOURLY, LAST 100 HOURS!!
+Function FetchDailyJSON($StockSymbol){
+    
+    // include connections page
+    include('connection.php');
+    
+    //Getting the json request into a PHP object
+    $Data = file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" . $StockSymbol . "&apikey=S7R0WLCOH163H1DE&datatype=json");
+    
+    //Decoding the json into a php array.
+    $JSONData = json_decode($Data);
+    
+    //Defining base query, will append each row to the query statement
+    $StartSQL = "INSERT INTO StockInfo.Time_Series_Daily(atr_stock_ID,Timestamp,Open,High,Low,Close,Volume)
+            VALUES('" . $StockSymbol . "',";
+    
+    //Counter to skip metadata
+    $count = 0;
+    
+    //Looping through each json object
+    foreach ($JSONData  as $JSONObject){
+        //Loop through each day
+        foreach ($JSONObject as $Timestamp => $TimeSeries){
+            //if not meta data, append the time stamp to insert
+            if($count > 4){
+                $MidQuery = $StartSQL;
+                $MidQuery = $MidQuery . "'" . $Timestamp . "',";
+            }
+            //loop through each value, open, high, low, close, volume, append to insert query
+            foreach ($TimeSeries as $ObjectHeader => $ObjectValue){
+                $MidQuery = $MidQuery . "'" . $ObjectValue . "',";
+            }
+            // If not meta data, append end of query statement
+            if($count > 4){
+                $MidQuery =  substr($MidQuery, 0, -2) . "');";
+                if ($conn->query($MidQuery) == TRUE){}
+            }
+            $count = $count + 1;
+        }
+    }
+}
+
+// this function will fetch and store intradaily values given the parameters
+//RETURNS 100 DATA POINTS, SO IF HOURLY, LAST 100 HOURS!!
+Function FetchDailyAdjustedJSON($StockSymbol){
+    
+    // include connections page
+    include('connection.php');
+    
+    //Getting the json request into a PHP object
+    $Data = file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" . $StockSymbol . "&apikey=S7R0WLCOH163H1DE&datatype=json");
+    
+    //Decoding the json into a php array.
+    $JSONData = json_decode($Data);
+    
+    //Defining base query, will append each row to the query statement
+    $StartSQL = "INSERT INTO StockInfo.Time_Series_Daily_Adjusted(atr_stock_ID,Timestamp,Open,High,Low,Close,Volume)
+            VALUES('" . $StockSymbol . "',";
+    
+    //Counter to skip metadata
+    $count = 0;
+    
+    //Looping through each json object
+    foreach ($JSONData  as $JSONObject){
+        //Loop through each day
+        foreach ($JSONObject as $Timestamp => $TimeSeries){
+            //if not meta data, append the time stamp to insert
+            if($count > 4){
+                $MidQuery = $StartSQL;
+                $MidQuery = $MidQuery . "'" . $Timestamp . "',";
+            }
+            //loop through each value, open, high, low, close, volume, append to insert query
+            foreach ($TimeSeries as $ObjectHeader => $ObjectValue){
+                $MidQuery = $MidQuery . "'" . $ObjectValue . "',";
+            }
+            // If not meta data, append end of query statement
+            if($count > 4){
+                $MidQuery =  substr($MidQuery, 0, -2) . "');";
+                if ($conn->query($MidQuery) == TRUE){}
+            }
+            $count = $count + 1;
+        }
+    }
+}
+
+// this function will fetch and store intradaily values given the parameters
+//RETURNS 100 DATA POINTS, SO IF HOURLY, LAST 100 HOURS!!
+Function FetchWeeklyJSON($StockSymbol){
+    
+    // include connections page
+    include('connection.php');
+    
+    //Getting the json request into a PHP object
+    $Data = file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_Weekly&symbol=" . $StockSymbol . "&apikey=S7R0WLCOH163H1DE&datatype=json");
+    
+    //Decoding the json into a php array.
+    $JSONData = json_decode($Data);
+    
+    //Defining base query, will append each row to the query statement
+    $StartSQL = "INSERT INTO StockInfo.Time_Series_Weekly(atr_stock_ID,Timestamp,Open,High,Low,Close,Volume)
+            VALUES('" . $StockSymbol . "',";
+    
+    //Counter to skip metadata
+    $count = 0;
+    
+    //Looping through each json object
+    foreach ($JSONData  as $JSONObject){
+        //Loop through each day
+        foreach ($JSONObject as $Timestamp => $TimeSeries){
+            //if not meta data, append the time stamp to insert
+            if($count > 4){
+                $MidQuery = $StartSQL;
+                $MidQuery = $MidQuery . "'" . $Timestamp . "',";
+            }
+            //loop through each value, open, high, low, close, volume, append to insert query
+            foreach ($TimeSeries as $ObjectHeader => $ObjectValue){
+                $MidQuery = $MidQuery . "'" . $ObjectValue . "',";
+            }
+            // If not meta data, append end of query statement
+            if($count > 4){
+                $MidQuery =  substr($MidQuery, 0, -2) . "');";
+                if ($conn->query($MidQuery) == TRUE){}
+            }
+            $count = $count + 1;
+        }
+    }
+}
+
+
 ?>
