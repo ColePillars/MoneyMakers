b040ededd4827fdac45e90b3c3d12fde34f24ce3
PeterIaconaOU <pmiacona@oakland.edu>
2018-02-24 20:53:55 +0000

    Fixed my subs panel alignment issues and updated stock page formatting

---
+++ resources/loggedinnav.php
@@ -0,0 +1,17 @@
+            <ul class="nav navbar-top-links navbar-right btn-lg">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-lg fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+						<li class="divider"></li>
+						<li><a href="../resources/logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+						</li>
+                    </ul>
+                </li>
+            </ul>
+
