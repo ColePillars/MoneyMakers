887d23f484a8102460d609f3f28a5c0546688ac0
cWelland96 <cwelland@oakland.edu>
2018-03-12 23:43:30 +0000

    squashed a bug where twitter stock wouldnt update, still uncertain

--- resources/stockfunctions.php
+++ resources/stockfunctions.php
@@ -481,15 +481,14 @@ function FetchCryptoMonthlyJSON($StockSymbol, $Market){
 }
 
 function FetchRSIJSON($StockSymbol, $Interval, $TimePeriod){
-    
+    ob_start();
     // include connections page
     include('connection.php');
     
     //Getting the json request into a PHP object
     $Data = file_get_contents("https://www.alphavantage.co/query?function=RSI&symbol=" . $StockSymbol . "&interval=" . $Interval . "&time_period="
         . $TimePeriod . "&series_type=close&apikey=S7R0WLCOH163H1DE&datatype=json");
-    
-    
+
     //Decoding the json into a php array.
     $JSONData = json_decode($Data);
     
@@ -500,6 +499,7 @@ function FetchRSIJSON($StockSymbol, $Interval, $TimePeriod){
     //Counter to skip metadata
     $count = 0;
     $limit = 0;
+    
     $all_query_ok = true;
     $conn->begin_transaction();
     
@@ -513,14 +513,13 @@ function FetchRSIJSON($StockSymbol, $Interval, $TimePeriod){
                 $MidQuery = $MidQuery . "'" . $Timestamp . "',";
             }
             //loop through each value, open, high, low, close, volume, append to insert query
-            if($limit < 30){
+            if($limit < 107){
                 foreach ($TimeSeries as $ObjectHeader => $ObjectValue){
                     $MidQuery = $MidQuery . "'" . $ObjectValue . "',";
                 }
                 // If not meta data, append end of query statement
                 if($count > 6){
                     $MidQuery =  substr($MidQuery, 0, -2) . "','" . $StockSymbol . "_" . $Timestamp . "');";
-                    //echo $MidQuery . "<br>";
                     $conn->query($MidQuery) ? null : $all_query_ok = false;
                 }
                 $count = $count + 1;
@@ -534,5 +533,6 @@ function FetchRSIJSON($StockSymbol, $Interval, $TimePeriod){
     
     //Rollback changes if ANY query fails
     else { $conn->rollback(); }
+
 }
 ?>
