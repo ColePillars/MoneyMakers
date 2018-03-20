0fafdea36fe2900ccb6ea62af838d8ee0e8e46a9
cWelland96 <cwelland@oakland.edu>
2018-03-20 00:20:36 +0000

    fixed a bug where subbing to stocks would break other stocks

--- resources/stockfunctions.php
+++ resources/stockfunctions.php
@@ -1,97 +1,14 @@
 <?php 
 // this function will fetch and store intradaily values given the parameters
-//RETURNS 100 DATA POINTS, SO IF HOURLY, LAST 100 HOURS!!
-//CSV is depreciated, use the JSON functions
-function FetchIntraDailyCSV($StockSymbol, $Interval){
-    //including db connectoin
-    include('connection.php');
-  
-    //URL to get api data, from user input
-    $data = file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" . $StockSymbol . "&interval=" . $Interval . "&apikey=S7R0WLCOH163H1DE&datatype=csv");
-    //Split the response into rows via each array element is an array
-    $rows = explode("\n",$data);
-    
-    //enable variable to skip headers
-    $HeaderSkip = 0;
-    
-    //Loop through each row
-    foreach($rows as $row) {
-        
-        //Skip the header row
-        if ($HeaderSkip == 0){
-            $HeaderSkip = 1;
-            goto HeaderSkip;
-        }
-        
-        //Split each row up into columns by comma
-        $DataFields[] = str_getcsv($row);
-        //loop through ech column
-        foreach ($DataFields as $DataColumn){
-            //Query for inserting each row 
-            $InsertStockRowSQL = "
-            INSERT INTO StockInfo.Time_Series_Intradaily(atr_stock_ID,Timestamp,Open,High,Low,Close,Volume)
-            VALUES ('" . $StockSymbol . "','" . $DataColumn[0] ."','" . $DataColumn[1] ."','" . $DataColumn[2] ."','" . $DataColumn[3] ."','" . $DataColumn[4] ."','" . $DataColumn[5] . "');";
-            //Execute SQL query to update user type
-            if ($conn->query($InsertStockRowSQL) == TRUE){}
-        }
-        //To skip the headers
-        HeaderSkip:
-    }
-}
-
-
-// this function will fetch and store intradaily values given the parameters
-//RETURNS 100 DATA POINTS, SO IF HOURLY, LAST 100 HOURS!!
-Function FetchIntraDailyJSON($StockSymbol, $Interval){
-    
-    // include connections page
-    include('connection.php');
-    
-    //Getting the json request into a PHP object
-    $Data = file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" . $StockSymbol . "&interval=". $Interval . "&apikey=S7R0WLCOH163H1DE&datatype=json");
-    
-    //Decoding the json into a php array.
-    $JSONData = json_decode($Data);
-    
-    //Defining base query, will append each row to the query statement
-    $StartSQL = "INSERT INTO StockInfo.Time_Series_Intradaily(atr_stock_ID,Timestamp,Open,High,Low,Close,Volume)
-            VALUES('" . $StockSymbol . "',";
-    
-    //Counter to skip metadata
-    $count = 0;
-    
-    //Looping through each json object
-    foreach ($JSONData  as $JSONObject){
-        //Loop through each day
-        foreach ($JSONObject as $Timestamp => $TimeSeries){
-            //if not meta data, append the time stamp to insert
-            if($count > 5){
-                $MidQuery = $StartSQL;
-                $MidQuery = $MidQuery . "'" . $Timestamp . "',";
-            }
-            //loop through each value, open, high, low, close, volume, append to insert query
-            foreach ($TimeSeries as $ObjectHeader => $ObjectValue){
-                $MidQuery = $MidQuery . "'" . $ObjectValue . "',";
-            }
-            // If not meta data, append end of query statement
-            if($count > 5){
-                $MidQuery =  substr($MidQuery, 0, -2) . "');";
-                if ($conn->query($MidQuery) == TRUE){}
-            }
-            $count = $count + 1;
-        }
-    }   
-}
-
-
 // this function will fetch and store daily values given the parameters
 //RETURNS 100 DATA POINTS, SO IF HOURLY, LAST 100 days!!
+
 Function FetchDailyJSON($StockSymbol){
     
     // include connections page
     include('connection.php');
     //Getting the json request into a PHP object
-    $Data = file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" . $StockSymbol . "&apikey=S7R0WLCOH163H1DE&datatype=json");
+    $Data = file_get_contents("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=compact&symbol=" . $StockSymbol . "&apikey=S7R0WLCOH163H1DE&datatype=json");
     
     //Decoding the json into a php array.
     $JSONData = json_decode($Data);
@@ -102,13 +19,10 @@ Function FetchDailyJSON($StockSymbol){
     
     //Counter to skip metadata
     $count = 0;
-    
-    //A boolean which is to be set to false if ANY query fails
-    $all_query_ok = true;
-    //Begins transaction, this turns off auto commits
-    $conn->begin_transaction();
-    
+     
+   
     //Looping through each json object
+    
     foreach ($JSONData  as $JSONObject){
         //Loop through each day
         foreach ($JSONObject as $Timestamp => $TimeSeries){
@@ -117,26 +31,25 @@ Function FetchDailyJSON($StockSymbol){
                 $MidQuery = $StartSQL;
                 $MidQuery = $MidQuery . "'" . $Timestamp . "',";
             }
-            //loop through each value, open, high, low, close, volume, append to insert query
-            foreach ($TimeSeries as $ObjectHeader => $ObjectValue){
-                $MidQuery = $MidQuery . "'" . $ObjectValue . "',";
-            }
-            // If not meta data, append end of query statement
-            if($count > 4){
-                $MidQuery =  substr($MidQuery, 0, -2) . "','" . $StockSymbol . "_" . $Timestamp . "');";
-                $conn->query($MidQuery) ? null : $all_query_ok = false;
+         
                 
-            }
-            $count = $count + 1;
+                //loop through each value, open, high, low, close, volume, append to insert query
+                foreach ($TimeSeries as $ObjectHeader => $ObjectValue){
+                    $MidQuery = $MidQuery . "'" . $ObjectValue . "',";
+                }
+                
+                // If not meta data, append end of query statement
+                if($count > 4){
+                    $MidQuery =  substr($MidQuery, 0, -2) . "','" . $StockSymbol . "_" . $Timestamp . "');";
+                   // echo $MidQuery . "<br>";
+                   $conn->query($MidQuery) ? null : $all_query_ok = false;
+                    
+                }
+
+                $count = $count + 1;
         }
-        
-        
+
     }
-    //Commits changes if ALL queries succeed
-    if ($all_query_ok) { $conn->commit(); }
-    
-    //Rollback changes if ANY query fails
-    else { $conn->rollback(); }
     
 }
 
--- resources/subtostock.php
+++ resources/subtostock.php
@@ -1,32 +1,59 @@
 <?php 
 //Start session
 session_start();
+
+
 //Includes
 include('../resources/connection.php');
 include('stockfunctions.php');
 include('buysellmethods.php');
-
+echo $_POST['Symbol'];
 
 //Insert record for stock subscription
 $SubToStockSQL = "INSERT INTO UserCredentials.tbl_stock_subs (atr_stocksubid,atr_username,atr_stock_id) VALUES ('" . $_SESSION['username'] . "_" . $_POST['Symbol'] . "','" . $_SESSION['username']  . "','" . $_POST['Symbol'] . "');";
 if ($conn->query($SubToStockSQL) == TRUE){}
 
-//Fetch daily stock market values
-FetchDailyJSON($_POST['Symbol']);
-//Fetch RSI values
-FetchRSIJSON($_POST['Symbol'], "daily", "100");
+//checking if there are stocks that need to be update
+$CheckOutdatedStockSQL = "
 
-//Determine buy/sell for Two_Period_RsE
-Two_Period_RSI();
-//Determine buy/sell for Heikin_Ashi
-Heikin_Ashi();
-//Make final buy/sell choice
-Final_Decision();
+SELECT DISTINCT s.atr_stock_id, d.timestamp
+FROM UserCredentials.tbl_stock_subs  as s
 
-//push user back to their stock page
-header('Location: ../pages/stockpage.php?Symbol=' . $_POST['Symbol']);
-exit();
+LEFT JOIN (
+
+SELECT atr_stock_id, MAX(timestamp) as'timestamp'
+    FROM StockInfo.Time_Series_Daily 
+
+    GROUP BY atr_stock_id
+) as d ON d.atr_stock_id = s.atr_stock_id
+WHERE d.timestamp <> DATE(NOW())
+ORDER BY atr_stock_id ASC
+
+";
+    
+$CheckOutdatedStockResult = mysqli_query($conn, $CheckOutdatedStockSQL);
+if ($CheckOutdatedStockResult->num_rows > 0){
+    while($row = $CheckOutdatedStockResult->fetch_assoc()) {
+        FetchDailyJSON($row['atr_stock_id']);
+        FetchRSIJSON($row['atr_stock_id'], "daily", "100");
+    }
+}
 
 
+    //Fetch daily information
+    FetchDailyJSON($_POST['Symbol']);
+    FetchRSIJSON($_POST['Symbol'], "daily", "100");
+    
+    //Determine buy/sell for Two_Period_RsE
+    Two_Period_RSI();
+    //Determine buy/sell for Heikin_Ashi
+    Heikin_Ashi();
+    //Make final buy/sell choice
+    Final_Decision();
+
+    //push user back to their stock page
+    header('Location: ../pages/stockpage.php?Symbol=' . $_POST['Symbol']);
+    exit();
+    
 
 ?>
